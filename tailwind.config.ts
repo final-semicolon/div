a9eb7143e589d7bd998e80d72b8330280bea6a8c
JvnKim <166312623+JvnKim@users.noreply.github.com>
2024-08-10 18:46:00 +0900

    feature: delete fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,6 @@ const config: Config = {
   theme: {
     extend: {
       fontFamily: {
-        // main: ['Pretendard', 'sans-serif'],
         pretendard: ['var(--font-pretendard)']
       },
       colors: {
