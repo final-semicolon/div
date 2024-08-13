8152902a5abcf2233f87eef52de2342b005e3f15
tpring <pororo1@naver.com>
2024-08-13 13:43:29 +0900

    update : serch history UI and delete

--- src/components/header/SearchBar.tsx
+++ src/components/header/SearchBar.tsx
@@ -1,18 +1,19 @@
-import SearchButton from '@/assets/images/header/SearchButton';
-import { isSearchValid } from '@/utils/validateBannedWords';
+import { useEffect, useRef, useState, KeyboardEvent } from 'react';
 import { useRouter } from 'next/navigation';
-import { KeyboardEvent, useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
-import { TAG_LIST } from '@/constants/tags';
+import SearchButton from '@/assets/images/header/SearchButton';
 import X from '@/assets/images/header/X';
+import { isSearchValid } from '@/utils/validateBannedWords';
+import { TAG_LIST } from '@/constants/tags';
 
 const SearchBar = () => {
   const router = useRouter();
   const [keyword, setKeyword] = useState('');
   const [showSuggestions, setShowSuggestions] = useState(false);
-  const [isFocused, setIsFocused] = useState<boolean>(false);
   const [filteredTags, setFilteredTags] = useState<string[]>([]);
+  const [isFocused, setIsFocused] = useState<boolean>(false);
   const [searchHistory, setSearchHistory] = useState<string[]>([]);
+  const searchBarRef = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
     if (keyword.startsWith('#')) {
@@ -23,22 +24,25 @@ const SearchBar = () => {
     } else {
       setShowSuggestions(false);
     }
-  }, [keyword]);
-
-  useEffect(() => {
     const savedHistory = localStorage.getItem('searchHistory');
     if (savedHistory) {
       setSearchHistory(JSON.parse(savedHistory));
     }
-  }, []);
+  }, [keyword]);
 
-  const saveSearchHistory = (newKeyword: string) => {
-    setSearchHistory((prevHistory) => {
-      const updatedHistory = [newKeyword, ...prevHistory.filter((item) => item !== newKeyword)].slice(0, 10);
-      localStorage.setItem('searchHistory', JSON.stringify(updatedHistory));
-      return updatedHistory;
-    });
-  };
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (searchBarRef.current && !searchBarRef.current.contains(event.target as Node)) {
+        setShowSuggestions(false);
+        setIsFocused(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
 
   const handleSearch = (e: KeyboardEvent<HTMLInputElement>) => {
     if (e.key === 'Enter') {
@@ -49,7 +53,6 @@ const SearchBar = () => {
         });
         return;
       }
-
       setShowSuggestions(false);
       setIsFocused(false);
 
@@ -63,28 +66,46 @@ const SearchBar = () => {
     }
   };
 
+  const saveSearchHistory = (newKeyword: string) => {
+    setSearchHistory((prevHistory) => {
+      const updatedHistory = [newKeyword, ...prevHistory.filter((item) => item !== newKeyword)].slice(0, 10);
+      localStorage.setItem('searchHistory', JSON.stringify(updatedHistory));
+      return updatedHistory;
+    });
+  };
+
+  const handleDeleteSearchHistory = (keywordToRemove: string) => {
+    setSearchHistory((prevHistory) => {
+      const updatedHistory = prevHistory.filter((item) => item !== keywordToRemove);
+      localStorage.setItem('searchHistory', JSON.stringify(updatedHistory));
+      return updatedHistory;
+    });
+  };
+
   const handleTagClick = (tag: string) => {
     const newKeyword = `#${tag}`;
     setKeyword(newKeyword);
-    router.push(`/search?searchType=tag&keyword=${newKeyword.slice(1)}`);
     setShowSuggestions(false);
+    setIsFocused(false);
+    router.push(`/search?searchType=tag&keyword=${newKeyword.slice(1)}`);
+  };
+
+  const handleHistoryClick = (item: string) => {
+    setKeyword(item);
+    if (item.startsWith('#')) {
+      router.push(`/search?searchType=tag&keyword=${item}`);
+    } else {
+      router.push(`/search?searchType=title&keyword=${item}`);
+    }
+    setIsFocused(false);
   };
 
   const handleClearKeyword = () => {
     setKeyword('');
   };
 
-  const handleHistoryClick = (item: string) => {
-    setKeyword(item);
-    if (item.startsWith('#')) {
-      router.push(`/search?searchType=tag&keyword=${item.slice(1)}`);
-    } else {
-      router.push(`/search?searchType=title&keyword=${item}`);
-    }
-  };
-
   return (
-    <div className="relative flex flex-col items-center text-neutral-400">
+    <div ref={searchBarRef} className="relative flex flex-col items-center text-neutral-400">
       <div className="flex items-center border border-neutral-200 rounded-md w-[318px] h-[56px]">
         <div className="flex items-center ml-4">
           <SearchButton />
@@ -95,7 +116,6 @@ const SearchBar = () => {
             onChange={(e) => setKeyword(e.target.value)}
             onKeyDown={handleSearch}
             onFocus={() => setIsFocused(true)}
-            onBlur={() => setIsFocused(false)}
             className="mx-2 pr-4 w-[222px] font-bold focus:outline-none bg-transparent"
           />
           {!keyword || (
@@ -105,12 +125,17 @@ const SearchBar = () => {
           )}
         </div>
       </div>
-      {isFocused && !showSuggestions && (
+      {isFocused && !showSuggestions && searchHistory.length > 0 && (
         <ul className="search-toggle-list">
           {searchHistory.map((item, index) => (
-            <li key={index} className="search-toggle-item" onClick={() => handleHistoryClick(item)}>
-              {item}
-            </li>
+            <div key={index} className="flex items-center justify-between">
+              <li className="search-toggle-item" onClick={() => handleHistoryClick(item)}>
+                {item}
+              </li>
+              {/* <button className="mb-4 mr-4" onClick={() => handleDeleteSearchHistory(item)}>
+                <X />
+              </button> */}
+            </div>
           ))}
         </ul>
       )}
