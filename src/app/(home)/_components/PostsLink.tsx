4fa05a649685147460f1ff216e2a4cc4c4f16675
Floweringworld <jns8665@naver.com>
2024-08-17 20:25:49 +0900

    fix: 이미지 순서 변경

--- src/app/(home)/_components/PostsLink.tsx
+++ src/app/(home)/_components/PostsLink.tsx
@@ -7,10 +7,6 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, Navigation } from 'swiper/modules';
 import { useEffect, useState } from 'react';
 import SwiperCore from 'swiper';
-import CarouselLeftHover from '@/assets/images/common/CarouselLeftHover';
-import CarouselLeft from '@/assets/images/common/CarouselLeft';
-import CarouselRightHover from '@/assets/images/common/CarouselRightHover';
-import CarouselRight from '@/assets/images/common/CarouselRight';
 import Image from 'next/image';
 import LeftIconHover from '@/assets/images/common/LeftIconHover';
 import LeftIcon from '@/assets/images/common/LeftIcon';
@@ -21,7 +17,6 @@ const PostsLink = () => {
   const [swiperInstance, setSwiperInstance] = useState<SwiperCore | null>(null);
   const [isBeginning, setIsBeginning] = useState(true);
   const [isEnd, setIsEnd] = useState(false);
-  const [isHoveringPrev, setIsHoveringPrev] = useState(false);
   const [isHoveringNext, setIsHoveringNext] = useState(false);
 
   useEffect(() => {
@@ -29,7 +24,6 @@ const PostsLink = () => {
       const handleSlideChange = () => {
         setIsBeginning(swiperInstance.isBeginning);
         setIsEnd(swiperInstance.isEnd);
-        setIsHoveringPrev(false);
         setIsHoveringNext(false);
       };
 
@@ -65,7 +59,14 @@ const PostsLink = () => {
       >
         <SwiperSlide>
           <Link href={'/qna'}>
-            <Image src={qna} alt="Q&A 게시판" width={1500} height={500} className="w-[1204px] h-[234px]" />
+            <Image
+              src={qna}
+              alt="Q&A 게시판"
+              loading="eager"
+              width={1500}
+              height={500}
+              className="w-[1204px] h-[234px]"
+            />
           </Link>
         </SwiperSlide>
         <SwiperSlide className="swiper-slide">
@@ -83,8 +84,6 @@ const PostsLink = () => {
         <div
           className="absolute top-1/2 transform -translate-y-1/2 flex justify-center items-center w-[52px] h-[52px] left-[-26px] shadow-button  bg-neutral-50 hover:bg-neutral-100 rounded-full  z-50"
           onClick={handlePrevClick}
-          onMouseEnter={() => setIsHoveringPrev(true)}
-          onMouseLeave={() => setIsHoveringPrev(false)}
         >
           {isHoveringNext ? <LeftIconHover /> : <LeftIcon />}
         </div>
@@ -93,8 +92,6 @@ const PostsLink = () => {
         <div
           className="absolute top-1/2 transform -translate-y-1/2 flex justify-center items-center w-[52px] h-[52px] right-[-26px] shadow-button  bg-neutral-50 hover:bg-neutral-100 rounded-full  z-50"
           onClick={handleNextClick}
-          onMouseEnter={() => setIsHoveringNext(true)}
-          onMouseLeave={() => setIsHoveringNext(false)}
         >
           {isHoveringNext ? <RightIconHover /> : <RightIcon />}
         </div>
